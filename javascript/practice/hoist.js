a577cd4057452c926d45c9a5f370b890f3a0c5b9
pauljsyi <pauljsyi@gmail.com>
2022-10-25 08:53:03 -0700

    convert to arrow and algos

--- javascript/practice/hoist.js
+++ javascript/practice/hoist.js
@@ -7,3 +7,14 @@
 // let example = "I'm the example!"
 // output: reference error: cannot access 'example' before initialization
 
+const array = ['a', 'b', 'c']
+
+const concat = ['z'].concat(array)
+console.log(array)
+console.log(concat);
+
+// array.unshift("runnning")
+// console.log(array)
+
+array.splice(0,0,"veal")
+console.log(array);
