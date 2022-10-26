242d6a261870d6983b490d8a72131d84a777532d
pauljsyi <pauljsyi@gmail.com>
2022-10-26 00:41:54 -0700

    convert class component to functional components

--- react/class-components/prop-it-up/src/App.jsx
+++ react/class-components/prop-it-up/src/App.jsx
@@ -8,25 +8,25 @@ function App() {
       <PersonCard
         last_name="Doe"
         first_name="Jane"
-        age="45"
+        age={45}
         hair_color="Black"
       />
       <PersonCard
         last_name="Smith"
         first_name="John"
-        age="88"
+        age={88}
         hair_color="Brown"
       />
       <PersonCard
         last_name="Fillmore"
         first_name="Millard"
-        age="50"
+        age={50}
         hair_color="Brown"
       />
       <PersonCard
         last_name="Smith"
         first_name="Maria"
-        age="62"
+        age={62}
         hair_color="Brown"
       />
     </div>
--- react/class-components/prop-it-up/src/components/PersonCard.jsx
+++ react/class-components/prop-it-up/src/components/PersonCard.jsx
@@ -1,31 +1,54 @@
-import React, { Component } from "react";
+// ========================
+// CLASS COMPONENT
+// =======================
+// import React, { Component } from "react";
 
-class PersonCard extends Component {
-  constructor(props) {
-    super(props);
+// class PersonCard extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      age: parseInt(this.props.age),
-    };
-  }
+//     this.state = {
+//       age: this.props.age,
+//     };
+//   }
 
-  older = () => {
-    // console.log(typeof this.state.age);
-    this.setState({ age: this.state.age + 1 });
-  };
+//   older = () => {
+//     // console.log(typeof this.state.age);
+//     this.setState({ age: this.state.age + 1 });
+//   };
 
-  render() {
-    return (
-      <>
-        <h1>
-          {this.props.last_name}, {this.props.first_name}
-        </h1>
-        <p>Age: {this.state.age}</p>
-        <button onClick={this.older}>birthday</button>
-        <p>Hair Color: {this.props.hair_color}</p>
-      </>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <>
+//         <h1>
+//           {this.props.last_name}, {this.props.first_name}
+//         </h1>
+//         <p>Age: {this.state.age}</p>
+//         <button onClick={this.older}>birthday</button>
+//         <p>Hair Color: {this.props.hair_color}</p>
+//       </>
+//     );
+//   }
+// }
+// export default PersonCard;
+
+// ===================
+// FUNCTIONAL COMPONENT
+// ===================
+
+import React from "react";
+
+const PersonCard = (props) => {
+  return (
+    <>
+      <h1>
+        {props.last_name}, {props.first_name}
+      </h1>
+      <p>Age: {props.age}</p>
+      <button>birthday</button>
+      <p>Hair Color: {props.hair_color}</p>
+    </>
+  );
+};
 
 export default PersonCard;
