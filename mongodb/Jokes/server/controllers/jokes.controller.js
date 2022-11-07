56f7142f37940e4ecf75bf119cf7525338b4505e
pauljsyi <pauljsyi@gmail.com>
2022-11-07 02:32:43 -0800

    jokes mongoose express setup

---
+++ mongodb/Jokes/server/controllers/jokes.controller.js
@@ -0,0 +1,45 @@
+const Joke = require("../models/jokes.model");
+
+module.exports.findAllJokes = (req, res) => {
+  Joke.find()
+    .then((allJokes) => res.json({ joke: allJokes }))
+    .catch((err) =>
+      res.json({ message: "something went terribly wrong", error: err })
+    );
+};
+
+module.exports.findOneJoke = (req, res) => {
+  Joke.findOne({ _id: req.params.id })
+    .then((oneJoke) => res.json({ joke: oneJoke }))
+    .catch((err) =>
+      res.json({ message: "Something went terribly wrong", error: err })
+    );
+};
+
+module.exports.findRandomJoke = (req, res) => {
+  // random joke
+};
+
+module.exports.createNewJoke = (req, res) => {
+  Joke.create(req.body)
+    .then((newlyCreatedJoke) => res.json({ joke: newlyCreatedJoke }))
+    .catch((err) =>
+      res.json({ message: "Something went terribly wrong", error: err })
+    );
+};
+
+module.exports.updateExistingJoke = (req, res) => {
+  Joke.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .then((updatedUser) => res.json({ joke: updatedUser }))
+    .catch((err) =>
+      res.json({ message: "something went terribly wrong", error: err })
+    );
+};
+
+module.exports.deleteAnExistingJoke = (req, res) => {
+  Joke.deleteOne({ _id: req.params.id })
+    .then((result) => res.json({ result: result }))
+    .catch((err) =>
+      res.json({ message: "Something went terribly wrong", error: err })
+    );
+};
